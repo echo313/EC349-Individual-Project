8c24312c98e12dd95e6d0d0370bd184a3ee261e3
echo313 <jjune1100@gmail.com>
2023-11-30 17:47:48 +0000

    Created project report and code script

---
+++ EC349 Individual Project.R
@@ -0,0 +1,36 @@
+#Setting GitHub repository
+
+
+#Pre-Processing Yelp Academic Data for the Assignment
+library(jsonlite)
+
+#Clear
+cat("\014")  
+rm(list=ls())
+
+#Set Directory as appropriate
+setwd("C:/Users/jjune/OneDrive/Desktop/EC349 Data Science for Economists/EC349 Individual Project")
+
+#Load Different Data
+
+business_data <- stream_in(file("yelp_academic_dataset_business.json")) 
+review_data  <- stream_in(file("yelp_academic_dataset_review.json")) 
+checkin_data  <- stream_in(file("yelp_academic_dataset_checkin.json")) 
+user_data <- stream_in(file("yelp_academic_dataset_user.json")) 
+tip_data  <- stream_in(file("yelp_academic_dataset_tip.json")) 
+
+# Save each dataset to an RDS file
+saveRDS(business_data, "business_data.rds")
+saveRDS(review_data, "review_data.rds")
+saveRDS(checkin_data, "checkin_data.rds")
+saveRDS(user_data, "user_data.rds")
+saveRDS(tip_data, "tip_data.rds")
+
+# Load the datasets from the saved RDS files
+business_data <- readRDS("business_data.rds")
+review_data <- readRDS("review_data.rds")
+checkin_data <- readRDS("checkin_data.rds")
+user_data <- readRDS("user_data.rds")
+tip_data <- readRDS("tip_data.rds")
+
+
