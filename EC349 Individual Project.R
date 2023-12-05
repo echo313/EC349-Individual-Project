ffba32810905329cd59075fa39be0267bd546ff7
echo313 <jjune1100@gmail.com>
2023-12-05 01:46:53 +0000

    Almost finished cleaning

--- EC349 Individual Project.R
+++ EC349 Individual Project.R
@@ -22,6 +22,7 @@ tip_data  <- stream_in(file("yelp_academic_dataset_tip.json"))
 saveRDS(business_data, "business_data.rds")
 saveRDS(checkin_data, "checkin_data.rds")
 saveRDS(tip_data, "tip_data.rds")
+saveRDS(user_data, "user_data.rds")
 
 # Load the datasets from the saved RDS files
 business_data <- readRDS("business_data.rds")
@@ -29,6 +30,7 @@ load("Yelp_review_small.Rda") # "review_data_small"
 checkin_data <- readRDS("checkin_data.rds")
 load("Yelp_user_small.Rda") # "user_data_small"
 tip_data <- readRDS("tip_data.rds")
+user_data <- readRDS("user_data.rds")
 
 # Exploring Data
 installed.packages()
@@ -272,3 +274,390 @@ str(branch_data)
 final_data <- branch_data
 
 str(final_data)
+
+final_data <- select(final_data, -categories)
+
+str(final_data)
+
+# Check NA's in state
+na_count_state <- sum(is.na(final_data$state))
+print(na_count_state)
+
+# Check for NA's in each variable
+na_count_user_id <- sum(is.na(final_data$user_id))
+print(na_count_user_id)
+
+na_count_business_id <- sum(is.na(final_data$business_id))
+print(na_count_business_id)
+
+na_count_review_id <- sum(is.na(final_data$review_id))
+print(na_count_review_id)
+
+final_data <- final_data %>% rename(stars_reviews = stars.x)
+
+na_count_stars_reviews <- sum(is.na(final_data$star_reviews))
+print(na_count_stars_reviews)
+
+final_data <- final_data %>% rename(useful_reviews = useful.x)
+
+na_count_useful_reviews <- sum(is.na(final_data$useful_reviews))
+print(na_count_useful_reviews)
+
+final_data <- final_data %>% rename(funny_reviews = funny.x)
+
+na_count_funny_reviews <- sum(is.na(final_data$funny_reviews))
+print(na_count_funny_reviews)
+
+final_data <- final_data %>% rename(cool_reviews = cool.x)
+
+na_count_cool_reviews <- sum(is.na(final_data$cool_reviews))
+print(na_count_cool_reviews)
+
+
+na_count_text <- sum(is.na(final_data$text))
+print(na_count_text)
+
+na_count_latitude <- sum(is.na(final_data$latitude))
+print(na_count_latitude)
+
+na_count_longitude <- sum(is.na(final_data$longitude))
+print(na_count_longitude)
+
+
+final_data <- final_data %>% rename(stars_business = stars.y)
+
+na_count_stars_business <- sum(is.na(final_data$stars_business))
+print(na_count_stars_business)
+
+final_data <- final_data %>% rename(review_count_business = review_count.x)
+
+na_count_review_count_business <- sum(is.na(final_data$review_count_business))
+print(na_count_review_count_business)
+
+na_count_is_open <- sum(is.na(final_data$is_open))
+print(na_count_is_open)
+
+final_data <- final_data %>% rename(review_count_user = review_count.y)
+
+na_count_review_count_user <- sum(is.na(final_data$review_count_user))
+print(na_count_review_count_user)
+# too many NAs
+
+
+zero_count <- sum(final_data$review_count_user == 0, na.rm = TRUE)
+print(zero_count)
+
+one_count <- sum(final_data$review_count_user == 1, na.rm = TRUE)
+print(one_count)
+
+summary(final_data$review_count_user)
+
+# I thought NAs were 0s, but that doesn't seem to be the case, I will have to take out this variable as it is incomplete
+# This is a shame as there could be a relationship between the amount of reviews a user has written and 'stars'
+
+final_data <- select(final_data, -review_count_user)
+# Removed review_count_user
+
+final_data <- final_data %>% rename(useful_sentbyuser = useful.y)
+
+na_count_useful_sentbyuser <- sum(is.na(final_data$useful_sentbyuser))
+# Interestingly, the number of NAs is the same as in review count. 
+
+final_data <- final_data %>% rename(funny_sentbyuser = funny.y)
+
+na_count_funny_sentbyuser <- sum(is.na(final_data$funny_sentbyuser))
+print(na_count_funny_sentbyuser)
+
+zero_count_funny_sentbyuser <- sum(final_data$funny_sentbyuser == 0, na.rm = TRUE)
+print(zero_count_funny_sentbyuser)
+# Again the same....
+
+final_data <- final_data %>% rename(cool_sentbyuser = cool.y)
+
+na_count_cool_sentbyuser <- sum(is.na(final_data$cool_sentbyuser))
+print(na_count_cool_sentbyuser)
+# As NA is not the same as 0.=, these variables should be eliminated
+
+final_data <- select(final_data, -cool_sentbyuser, -useful_sentbyuser, -funny_sentbyuser)
+
+# Define a custom function to count the number of elite years
+count_elite_years <- function(elite_years) {
+  if (is.na(elite_years) || elite_years == "") {
+    # Return 0 if the entry is NA or an empty string
+    return(0)
+  } else {
+    # Split the string by commas and count the elements
+    years <- strsplit(elite_years, ",")[[1]]
+    return(length(years))
+  }
+}
+
+# Apply the function to the elite column
+final_data$elite_years_count <- sapply(final_data$elite, count_elite_years)
+
+# Check the results
+head(final_data$elite_years_count)
+
+summary(final_data$elite_years_count)
+
+highest_elite_years_count <- max(final_data$elite_years_count, na.rm = TRUE)
+print(highest_elite_years_count)
+
+table(final_data$elite_years_count)
+
+table(final_data$elite)
+
+# Now delete elite and friends, as they are no longer needed
+final_data <- select(final_data, -elite, -friends)
+
+# Back to counting NA
+na_count_fans <- sum(is.na(final_data$fans))
+print(na_count_fans)
+
+zero_count_fans <- sum(final_data$fans == 0, na.rm = TRUE)
+print(zero_count_fans)
+# Again the NA value is the same as above, making me think these functions were relatively new and hence didn't 
+# apply to older reviews/users
+
+# Still get rid of variable..
+final_data <- select(final_data, -fans)
+
+
+na_count_average_stars <- sum(is.na(final_data$average_stars))
+print(na_count_average_stars)
+
+summary(final_data$average_stars)
+
+na_count_average_stars_smalldata <- sum(is.na(user_data_small$average_stars))
+print(na_count_average_stars_smalldata)
+
+
+
+# Assuming you're merging on a column named 'user_id'
+# Replace 'user_id' with the actual key column name
+
+# I merged the dataset wrong, need to merge again...
+
+# Merging review data with user data
+review_user_merge <- merge(review_data_small, user_data_small, by = "user_id", all.x = TRUE)
+
+# Merging the above result with business data
+final_data2 <- merge(review_user_merge, business_data, by = "business_id", all.x = TRUE)
+
+na_count_average_stars2 <- sum(is.na(final_data2$average_stars))
+print(na_count_average_stars2)
+
+
+# It turns out i didn't merge the dataset wrong, but rather because the dataset was subsetted from a larger dataset
+# There were inconsistent pairings, in that there were user id's in the review_data_small dataset that were not 
+# Present in the user_data_small dataset. 
+
+# Remerging the dataset with full user_data
+
+user_data <- readRDS("user_data.rds")
+
+review_user_merge2 <- merge(review_data_small, user_data, by = "user_id", all.x = TRUE)
+final_data3 <- merge(review_user_merge2, business_data, by = "business_id", all.x = TRUE)
+
+final_data3 <- select(final_data3, -name.x,-name.y, -address, -city)
+
+final_data3 <- select(final_data3, -postal_code, -attributes, -hours,-date)
+
+final_data3 <- final_data3 %>% rename(stars = stars.x)
+
+final_data3 <- final_data3 %>% rename(stars_business = stars.y)
+
+final_data3 <- final_data3 %>% rename(useful_reviews = useful.x)
+
+final_data3 <- final_data3 %>% rename(funny_reviews = funny.x)
+
+final_data3 <- final_data3 %>% rename(cool_reviews = cool.x)
+
+final_data3 <- final_data3 %>% rename(review_count_user = review_count.x)
+
+final_data3 <- final_data3 %>% rename(useful_sentbyuser = useful.y)
+
+final_data3 <- final_data3 %>% rename(funny_sentbyuser = funny.y)
+
+final_data3 <- final_data3 %>% rename(cool_sentbyuser = cool.y)
+
+final_data3 <- final_data3 %>% rename(review_count_business = review_count.y)
+
+summary(final_data3$review_count_business)
+
+summary(final_data3$review_count_user)
+
+
+# Filter categories again
+split_categories <- strsplit(final_data3$categories, ",")
+all_categories <- trimws(unlist(split_categories))
+category_counts <- table(all_categories)
+threshold <- nrow(branch_data) / 50
+significant_categories <- names(category_counts[category_counts >= threshold])
+
+filter_categories <- function(category_list) {
+  filtered <- category_list[category_list %in% significant_categories]
+  return(paste(filtered, collapse = ", "))
+}
+
+final_data3$filtered_categories <- sapply(strsplit(final_data3$categories, ", "), filter_categories)
+
+head(final_data3)
+
+# Split the categories and unlist them into a single vector
+split_categories <- strsplit(final_data3$filtered_categories, ",")
+all_categories <- unlist(split_categories)
+
+# Trim any leading or trailing spaces
+all_categories <- trimws(all_categories)
+
+# Create a frequency table of individual categories
+category_counts_individual <- table(all_categories)
+
+# View the table
+print(category_counts_individual)
+
+final_data3 <- select(final_data3, -categories)
+
+# Check for NAs
+
+na_count_business_id <- sum(is.na(final_data3$business_id))
+print(na_count_business_id)
+
+na_count_user_id <- sum(is.na(final_data3$user_id))
+print(na_count_user_id)
+
+na_count_review_id <- sum(is.na(final_data3$review_id))
+print(na_count_review_id)
+
+na_count_stars <- sum(is.na(final_data3$stars))
+print(na_count_stars)
+
+na_count_useful_reviews <- sum(is.na(final_data3$useful_reviews))
+print(na_count_useful_reviews)
+
+na_count_funny_reviews <- sum(is.na(final_data3$funny_reviews))
+print(na_count_funny_reviews)
+
+na_count_cool_reviews <- sum(is.na(final_data3$cool_reviews))
+print(na_count_cool_reviews)
+
+na_count_text <- sum(is.na(final_data3$text))
+print(na_count_text)
+
+na_count_review_count_user <- sum(is.na(final_data3$review_count_user))
+print(na_count_review_count_user)
+
+# Checking number of rows
+nrow(final_data3)
+
+# Excluding rows with NA for review_count_user
+final_data3 <- final_data3[!is.na(final_data3$review_count_user), ]
+
+nrow(final_data3)
+
+# Checking for NAs again
+na_count_yelping_since <- sum(is.na(final_data3$yelping_since))
+print(na_count_yelping_since)
+
+na_count_useful_sentbyuser<- sum(is.na(final_data3$useful_sentbyuser))
+print(na_count_useful_sentbyuser)
+
+na_count_funny_sentbyuser<- sum(is.na(final_data3$funny_sentbyuser))
+print(na_count_funny_sentbyuser)
+
+na_count_cool_sentbyuser<- sum(is.na(final_data3$cool_sentbyuser))
+print(na_count_cool_sentbyuser)
+
+na_count_elite<- sum(is.na(final_data3$elite))
+print(na_count_elite)
+
+# Values are not given as NA and instead " "
+head(final_data3$elite)
+
+# Changing elite so that it counts number of years elite.
+count_elite_years <- function(elite_string) {
+  if (elite_string == "") {
+    # Return 0 if the string is empty
+    return(0)
+  } else {
+    # Split the string by commas and count the elements
+    years <- unlist(strsplit(elite_string, ","))
+    return(length(years))
+  }
+}
+
+# Apply the function to the elite column
+final_data3$elite <- sapply(final_data3$elite, count_elite_years)
+
+# Check the results
+head(final_data3$elite)
+
+summary(final_data3$elite)
+
+# Checking for NA again
+na_count_friends<- sum(is.na(final_data3$friends))
+print(na_count_friends)
+
+na_count_fans<- sum(is.na(final_data3$fans))
+print(na_count_fans)
+
+final_data3 <- select(final_data3, -yelping_since)
+
+na_count_average_stars<- sum(is.na(final_data3$average_stars))
+print(na_count_average_stars)
+
+na_count_compliment_hot<- sum(is.na(final_data3$compliment_hot))
+print(na_count_compliment_hot)
+
+na_count_compliment_more<- sum(is.na(final_data3$compliment_more))
+print(na_count_compliment_more)
+
+na_count_compliment_profile<- sum(is.na(final_data3$compliment_profile))
+print(na_count_compliment_profile)
+
+na_count_compliment_cute<- sum(is.na(final_data3$compliment_cute))
+print(na_count_compliment_cute)
+
+na_count_compliment_list<- sum(is.na(final_data3$compliment_list))
+print(na_count_compliment_list)
+
+na_count_compliment_note<- sum(is.na(final_data3$compliment_note))
+print(na_count_compliment_note)
+
+na_count_compliment_plain<- sum(is.na(final_data3$compliment_plain))
+print(na_count_compliment_plain)
+
+na_count_compliment_cool<- sum(is.na(final_data3$compliment_cool))
+print(na_count_compliment_cool)
+
+final_data3 <- select(final_data3, -friends)
+
+na_count_compliment_funny<- sum(is.na(final_data3$compliment_funny))
+print(na_count_compliment_funny)
+
+na_count_compliment_writer<- sum(is.na(final_data3$compliment_writer))
+print(na_count_compliment_writer)
+
+na_count_compliment_photos<- sum(is.na(final_data3$compliment_photos))
+print(na_count_compliment_photos)
+
+na_count_state<- sum(is.na(final_data3$state))
+print(na_count_state)
+
+na_count_latitude<- sum(is.na(final_data3$latitude))
+print(na_count_latitude)
+
+na_count_longitude<- sum(is.na(final_data3$longitude))
+print(na_count_longitude)
+
+na_count_stars_business<- sum(is.na(final_data3$stars_business))
+print(na_count_stars_business)
+
+na_count_review_count_business<- sum(is.na(final_data3$review_count_business))
+print(na_count_review_count_business)
+
+na_count_is_open<- sum(is.na(final_data3$is_open))
+print(na_count_is_open)
+
+
